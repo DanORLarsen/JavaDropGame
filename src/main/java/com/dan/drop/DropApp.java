364451325a9e8c6ada45210a84d721ea192995e9
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-01-09 15:26:31 +0100

    First "Working" App Wont accept Entities.builder().viewFronNode method

---
+++ src/main/java/com/dan/drop/DropApp.java
@@ -0,0 +1,84 @@
+package com.dan.drop;
+
+
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.Map;
+
+public class DropApp extends GameApplication {
+    private Entity player;
+    @Override
+    protected void initSettings(com.almasb.fxgl.app.GameSettings gameSettings) {
+        gameSettings.setMenuEnabled(false);
+        gameSettings.setIntroEnabled(false);
+        gameSettings.setWidth(600);
+        gameSettings.setHeight(600);
+        gameSettings.setTitle("Basic Game App");
+        gameSettings.setVersion("0.1");
+    }
+    @Override
+    protected void initGame() {
+        player = Entities.builder()
+                .at(300, 300)
+                .viewFromNode(new Rectangle(25,25, Color.BLUE))
+                .buildAndAttach(player.getWorld());
+    }
+    @Override
+    protected void initInput() {
+        Input input = new Input();
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); // move right 5 pixels
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); // move left 5 pixels
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); // move up 5 pixels
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); // move down 5 pixels
+            }
+        }, KeyCode.S);
+    }
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+    }
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
