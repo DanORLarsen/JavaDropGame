d098ce6c7d07d17f667880a54cadef6554901697
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-01-10 01:21:30 +0100

    Java runtime fixed, Javafx.controls not found.. Cant import

--- src/main/java/com/dan/drop/DropApp.java
+++ src/main/java/com/dan/drop/DropApp.java
@@ -7,10 +7,7 @@ import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
-
 
 import java.util.Map;
 
@@ -76,7 +73,7 @@ public class DropApp extends GameApplication {
     protected void initGame() {
         player = Entities.builder()
                 .at(300, 300)
-                .viewFromNode(new Rectangle(25, 25, Color.BLUE))
+                .viewFromTexture("fxgl_icon.png")
                 .buildAndAttach(getGameWorld());
     }
 
